e5c4335112b6bf735353c15cef2dd09268afacf7
Carmeloanthony1 <meloproacc@gmail.com>
2026-07-31 10:12:30 -0400

    Konfigurasi tailwind + React

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState  } from "react";
+import './App.css';
+import './index.css'; 
+
+function App() {
+  return (
+    <div className= "min-h-screen bg-slate-900 text-slate-100 flex flex-col items-center justify-center">
+      <p>hai</p>
+    </div>
+  );
+}
+
+export default App;
