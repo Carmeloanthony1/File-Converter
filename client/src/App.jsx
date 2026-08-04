2733abbd647dcdc3b04a06c069faf6313732354e
Carmeloanthony1 <meloproacc@gmail.com>
2026-08-04 04:40:27 -0400

    final box opsi

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -85,8 +85,8 @@ function App() {
             </div>
             <svg className= "w-8 h-8"xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 15 15"><title xmlns="">arrow</title><path fill="currentColor" d="M8.293 2.293a1 1 0 0 1 1.414 0l4.5 4.5a1 1 0 0 1 0 1.414l-4.5 4.5a1 1 0 0 1-1.414-1.414L11 8.5H1.5a1 1 0 0 1 0-2H11L8.293 3.707a1 1 0 0 1 0-1.414"/></svg>
             <select value = {targetFORMAT} onChange={(e) => setTargetFORMAT(e.target.value)}
-              className="bg-[#808080] focus:bg-[#808080] active:bg-[#808080] p-2 rounded-lg w-22 h-12 flex text-center items-center">{FILE_OPTION(realFORMAT).map((format, index) => (
-                <option key = {index} value = {format} className="bg-[#555] text-white font-normal flex text-center">{format}</option>
+              className="bg-[#808080] p-2 rounded-lg w-22 h-12 flex text-center justify-center items-center">{FILE_OPTION(realFORMAT).map((format, index) => (
+                <option key = {index} value = {format} className="text-white font-normal flex text-center">{format}</option>
               ))}
             </select>
 
