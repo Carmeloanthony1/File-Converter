81a7c801aebd98f4aa373ba0146f6ee70ecda23c
Carmeloanthony1 <meloproacc@gmail.com>
2026-08-06 12:01:44 -0400

    add: menambahkan handle untuk subimit

--- client/src/Login.jsx
+++ client/src/Login.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { data, useNavigate } from 'react-router-dom';
 
 export default function Login(){
     const navigate = useNavigate();
@@ -13,7 +13,39 @@ export default function Login(){
 
     const handle_input = (e) => {
         setForminput({
-            ...forminput, [e.target.name]: e.target.value;
+            ...forminput, [e.target.name]: e.target.value
         });
+    };
+
+    const handle_submit = async (e) => {
+        e.preventDefault();
+        setErrormessage('');
+        setLoading(true);
+        try{
+            const response = await fetch('http://localhost:3000/api/auth/login', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' }, 
+                body: JSON.stringify({
+                    email:  forminput.email,
+                    password: forminput.password
+                })
+            });
+
+            const data = await response.json();
+            if(response.ok){
+                alert("Login sukses");
+                if(data.token){
+                    localStorage.setItem('access_token', data.token);
+                }
+                navigate('/');
+            } else {
+                setErrormessage(data.message || "Email atau password salah");
+            }
+        } catch(error) {
+            console.error('error saat login', error);
+            setErrormessage('Terjadi kesalahan server');
+        } finally {
+            setLoading(false);
+        }
     };
 }
