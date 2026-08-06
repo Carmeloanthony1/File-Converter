553ab41a4345bfe56aa04ab021af0e803c4c0ac5
Carmeloanthony1 <meloproacc@gmail.com>
2026-08-06 11:56:46 -0400

    add : menambahkan handle input

--- client/src/Login.jsx
+++ client/src/Login.jsx
@@ -2,5 +2,18 @@ import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 export default function Login(){
-    
+    const navigate = useNavigate();
+    const [forminput, setForminput] = useState({
+        email: '',
+        password: ''
+    });
+
+    const [loading, setLoading] = useState(false);
+    const [errormessage, setErrormessage] = useState("");
+
+    const handle_input = (e) => {
+        setForminput({
+            ...forminput, [e.target.name]: e.target.value;
+        });
+    };
 }
