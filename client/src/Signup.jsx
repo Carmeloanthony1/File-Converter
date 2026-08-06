b74e68263aa746e7a8f58e3a961e423f19acc031
Carmeloanthony1 <meloproacc@gmail.com>
2026-08-06 08:08:11 -0400

    draft design untuk sign up

--- client/src/Signup.jsx
+++ client/src/Signup.jsx
@@ -59,7 +59,33 @@ export default function Signup(){
     };
     return (
         <div className="min-h-screen bg-[#333] text-white flex items-center justify-center p-4">
-            <h2>test</h2>
+            <div className="bg-[#444]/90 backdrop-blur-md border-2 border-white/40 h-140 w-lg flex flex-col justify-center gap-4 p-5">
+                <h1 className="text-3xl font-bold flex flex-start justify-center">Sign up</h1>
+                {errormessage && (
+                    <div className="bg-red-500/20 border border-red-500 text-red-300 p-3 rounded-lg text-sm text-center">
+                        {errormessage}
+                    </div>
+                )}
+                <form onSubmit={handle_Submit} className="flex flex-col gap-4 justify-center items-center">
+                    <input 
+                        type = "text" name = "username" placeholder="Masukan username anda..." value = {forminput.username}
+                        onChange={handle_Input} className="p-3 w-sm bg-[#F0EFE7]/30 rounded-lg border-white-400 border-2 text-white text-lg">
+                    </input>
+                    <input 
+                        type = "email" name = "email" placeholder="Masukan email anda..." value = {forminput.email}
+                        onChange={handle_Input} className="p-3 w-sm bg-[#F0EFE7]/30 rounded-lg border-white-400 border-2 text-white text-lg">
+                    </input>
+                    <input 
+                        type = "password" name = "password" placeholder="Masukan password anda..." value = {forminput.password}
+                        onChange={handle_Input} className="p-3 w-sm bg-[#F0EFE7]/30 rounded-lg border-white-400 border-2 text-white text-lg">
+                    </input>
+                    <input 
+                        type = "password" name = "confirm_password" placeholder="Masukan password anda kembali" value = {forminput.confirm_password}
+                        onChange={handle_Input} className="p-3 w-sm bg-[#F0EFE7]/30 rounded-lg border-white-400 border-2 text-white text-lg">
+                    </input>
+                    <button type = "submit" disabled = {loading} className= "mt-4 p-3 w-30 rounded-lg border-white-400 border-2 bg-[#808080]">{loading ? 'Mohon menunggu' : 'SIGN UP'}</button>
+                </form>
+            </div>
         </div>
     )
 }
