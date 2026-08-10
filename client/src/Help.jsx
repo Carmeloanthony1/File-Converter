0aca577bf9843a51abb865b6b424a372a2403f0e
Carmeloanthony1 <meloproacc@gmail.com>
2026-08-10 06:53:16 -0400

    add: menghubungkan email dan helpdesk, jadi semua keluh kesah user langsung jadi email nanti

--- client/src/Help.jsx
+++ client/src/Help.jsx
@@ -11,6 +11,29 @@ export default function Help(){
 
     const [lastoken, setLasttoken] = useState(null);
 
+    const handle_submit = (e) => {
+        e.preventDefault();
+        if(!formdata.name || !formdata.message) return;
+        const randomNum = Math.floor(10000 + Math.random() * 90000);
+        const ticket = `Tict-${randomNum}`;
+
+        const subject = encodeURIComponent(`[Support Ticket ${ticket} - ${formdata.category}]`);
+        const bodytext = 
+        `
+        [Ticket : ${ticket}]
+        Name: ${formdata.name} 
+        Email: ${formdata.user_email}
+        Category: ${formdata.category}
+        
+        Problem Description: 
+        ${formdata.message}`;
+
+        const body = encodeURIComponent(bodytext);
+        window.location.href = `mailto:${Dev_email}?subject=${subject}&body=${body}`;
+        //otomatis bikin email biar user langsung bisa kirim aja 
+        setLasttoken(ticket);
+    };
+
     return (
     <div className="min-h-screen bg-[#333] text-slate-100 flex flex-col overflow-y-auto">
       {/* Navbar */}
@@ -27,10 +50,9 @@ export default function Help(){
       <main className="w-full mt-[60px] flex-1 flex justify-center items-center p-6 sm:p-10">
         <div className="w-full max-w-4xl bg-[#444] border-2 border-slate-300 rounded-2xl p-10 text-center shadow-2xl my-auto">
             <h2 className="text-3xl font-bold mb-2">Help Desk</h2>
-            <p className="mb-8 text-slate-300 text-lg">Submit your issue to dev</p>
+            <p className="mb-8 text-slate-300 text-lg">Submit Your Issue to dev</p>
 
-            {/* Form Grid 2 Kolom */}
-            <form className="grid grid-cols-1 md:grid-cols-2 gap-8 text-left">
+            <form id = "help-form" onSubmit = {handle_submit} className="grid grid-cols-1 md:grid-cols-2 gap-8 text-left">
                 
                 {/* KOLOM KIRI */}
                 <div className="flex flex-col gap-6">
@@ -45,7 +67,7 @@ export default function Help(){
                     <div>
                         <label className="text-base font-bold mb-2 block text-slate-200">Email</label>
                         <input 
-                            type="text" required placeholder="Your email" value={formdata.user_email} 
+                            type="email" required placeholder="Your email" value={formdata.user_email} 
                             onChange={(e) => setFormdata({ ...formdata, user_email: e.target.value})}
                             className="w-full bg-[#333] border border-slate-400 rounded-xl p-4 text-base focus:outline-none focus:border-slate-200"/>
                     </div>
@@ -74,7 +96,7 @@ export default function Help(){
                 </div>
             </form>
             <div className="flex justify-center mt-10">
-                <button className="w-40 h-15 bg-[#333] rounded-xl border border-slate-400 text-base focus:outline-none focus:border-slate-200 resize-none hover:scale-105 cursor-pointer ">Send</button>
+                <button type = "submit" form = "help-form" className="w-40 h-15 bg-[#333] rounded-xl border border-slate-400 text-base focus:outline-none focus:border-slate-200 resize-none hover:scale-105 cursor-pointer ">Send</button>
             </div>
         </div>
       </main>
