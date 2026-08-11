253f8ae1e9ee13e65e94292463f080e8d620488f
Carmeloanthony1 <meloproacc@gmail.com>
2026-08-11 00:17:29 -0400

    add:menambahkan seluruh pilihan convert ke footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,13 +1,40 @@
+import { title } from "framer-motion/client";
 import { Link } from "react-router-dom";
 export default function Footer(){
     const footer_section = [
         {
-            title: "Video Konverter",
+            title: "Video Converter",
             links: [
                 {label: "Mp4 Converter", to: "#"},
                 {label: "Video to GIF", to: "#"},
                 {label: "MOV to Mp4", to: "#"}
-            ]
-        }
-    ]
+            ],
+        },
+        {
+            title: "Audio Converter",
+            links: [
+                {label: "Mp3 Converter", to: "#"},
+                {label: "Mp4 to Mp3", to: "#"},
+            ],
+        },
+        {
+            title: "Image Converter",
+            links: [
+                {label: "JPG to PDF", to: "#"},
+                {label: "PDF to JPG", to: "#"},
+                {label: "HEIC to JPG", to: "#"},
+                {label: "Image to PDF", to: "#"},
+            ],
+        },
+        {
+            title: "Document Converter",
+            links: [
+                {label: "PDF to WORD", to: "#"},
+                {label: "EPUB to PDF", to: "#"},
+                {label: "EPUB to MOBI", to: "#"},
+                {label: "WORD to PDF", to: "#"},
+                {label: "DOCS to PDF", to: "#"},
+            ],
+        },
+    ];
 }
