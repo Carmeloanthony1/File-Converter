f1adbc2e92d31fca29a666f59638efcf56919e9e
Carmeloanthony1 <meloproacc@gmail.com>
2026-08-11 00:12:01 -0400

    add: menambahkan footer beserta pilihan convert nya

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+export default function Footer(){
+    const footer_section = [
+        {
+            title: "Video Konverter",
+            links: [
+                {label: "Mp4 Converter", to: "#"},
+                {label: "Video to GIF", to: "#"},
+                {label: "MOV to Mp4", to: "#"}
+            ]
+        }
+    ]
+}
