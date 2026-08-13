1643b9218bdb91484841fc6f11ce9721b67470c4
Carmeloanthony1 <meloproacc@gmail.com>
2026-08-13 05:49:01 -0400

    rework: navbar desktop dan hp

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
-const navbar = () => {
+const Navbar = () => {
     const [isOpen, setIsOpen] = useState(false);
     return (
         <nav className="bg-[#333] text-white sticky top-0 z-50">
@@ -17,12 +17,30 @@ const navbar = () => {
                     </svg>
                 </button>
                 {/* Buat desktop navbarnya*/}
-                <div className="hidden md: flex item-center text-white space-x-6 font-medium text-xm">
-                    <Link to="/" className='hover: text-blue-300 transition'>About</Link>
-                    <Link to="/convert" className='hover: text-blue-300 transition'>Convert</Link>
-                    <Link to="/help" className='hover: text-blue-300 transition'>Help</Link>
+                <div className="hidden md:flex items-center text-white space-x-6 font-medium text-sm">
+                    <Link to="/" className='hover:text-blue-300 transition'>About</Link>
+                    <Link to="/convert" className='hover:text-blue-300 transition'>Convert</Link>
+                    <Link to="/help" className='hover:text-blue-300 transition'>Help</Link>
                 </div>
             </div>
+        {isOpen && ( //biar nanti pas di pencet menunya, dia ga nutupin
+            <div className="md:hidden bg-[#808080] px-4 pt-2 space-y-2">
+                <Link to = "/" 
+                    onClick={() => setIsOpen(false)}
+                    className="block py-2 text-white hover:text-blue-300 font-medium"
+                >About</Link>
+                <Link to = "/convert" 
+                    onClick={() => setIsOpen(false)}
+                    className="block py-2 text-white hover:text-blue-300 font-medium"
+                >Convert</Link>
+                <Link to = "/help" 
+                    onClick={() => setIsOpen(false)}
+                    className="block py-2 text-white hover:text-blue-300 font-medium"
+                >Help</Link>
+            </div>
+        )}
         </nav>
-    )
-}
+    );
+};
+
+export default Navbar;
