435d829bb3afe04ba77d148f425c121ffdad21c3
Carmeloanthony1 <meloproacc@gmail.com>
2026-08-13 07:01:17 -0400

    install tailwind animate

--- client/src/About.jsx
+++ client/src/About.jsx
@@ -27,16 +27,9 @@ export default function About() {
   }, []);
 
   return (
-    <div className="min-h-screen bg-[#333] text-slate-100 flex flex-col overflow-y-auto">
+    <div className="min-h-screen bg-[#333] text-white">
       {/* Navbar */}
-      <nav className="w-full bg-[#808080] h-[60px] flex items-center p-5 justify-between fixed top-0 left-0 z-50">
-        <h1 className="text-xl font-bold">File Converter</h1>
-        <div className="flex items-center gap-12 font-bold">
-          <Link to="/about" className="text-xl font-bold">About</Link>
-          <Link to="/" className="text-xl font-bold">Convert</Link>
-          <Link to="/help" className="text-xl font-bold">Help</Link>
-        </div>
-      </nav>
+      <Navbar/>
 
       {/* Main */}
       <main className="w-full mt-[60px] flex flex-col">
