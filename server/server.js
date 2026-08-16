1bb9008f498f78e232f91ef92010b91ceaf15128
Carmeloanthony1 <meloproacc@gmail.com>
2026-08-16 01:28:47 -0400

    add: server rework, sudah siap untuk deploy

--- server/server.js
+++ server/server.js
@@ -8,27 +8,24 @@ const crypto = require('crypto');
 const convert_route = require('./convert');
 
 const app = express();
-const port = 3000;
-
-const access_secret = "access_secret";
+const access_secret = process.env.JWT_SECRET || "access_secret";
 
 app.use(cors({
-    origin: 'http://localhost:5173',
-    credentials : true //dia kasih izin ke port 5173 buat nanti tukeran data/komunikasi
+    origin:true,
+    credentials: true
 }));
 
 app.use('/converted', express.static(path.join(__dirname, 'converted')));
 app.use(express.json());
-app.use('/api', convert_route);
 
-const uploadDir = path.join(__dirname, 'uploads'); //kalau nanti ga ada foldernya, dia bikin sendiri
+const uploadDir = path.join('/tmp', 'uploads'); //kalau nanti ga ada foldernya, dia bikin sendiri
 if(!fs.existsSync(uploadDir)){
-    fs.mkdirSync(uploadDir);
+    fs.mkdirSync(uploadDir, { recursive: true });
 }
 
 const storage = multer.diskStorage({
     destination: (req, file, callback) => {
-        callback(null, 'uploads/');
+        callback(null, uploadDir);
     },
     filename: (req, file, callback) => {
         const uniquename = crypto.randomUUID() + '-' + file.originalname;
@@ -60,8 +57,14 @@ const verifytoken = (req, res, next) => {
 };
 
 app.locals.verifytoken = verifytoken;
-app.use('/api', verifytoken, convert_route);
 
-app.listen(port, () => {
-    console.log(`Server terkoneksi di port ${port}`);
-});
+app.use('/api', convert_route);
+
+if(process.env.NODE_ENV !== 'production'){
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => {
+        console.log(`Server terkoneksi di port ${PORT}`);
+    });
+}
+
+module.exports = app;
